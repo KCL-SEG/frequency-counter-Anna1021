fa8196e3053a3ca957462faedea6f25f31913d7e
Anna1021 <k22008684@kcl.ac.uk>
2023-10-05 09:33:23 +0100

    update codes

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,19 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        key = str(item)
+
+        if key not in frequencies:
+            frequencies[key] = 1
+        else:
+            frequencies[key] += 1
+
     return frequencies
+
+
+testResult1 = frequencies([100, "hello", "100", "100", "hello"])
+testResult2 = frequencies([200, "Hello", "200", "200", "hello"])
+print(testResult1)
+print(testResult2)
+
